e0597432f5794936bdc5081345c99581b533ceb4
charsmit <161141106+CharSmit@users.noreply.github.com>
2024-09-09 10:17:07 +0100

    Added encryption key generation, and password encryption

--- secureLogin.py
+++ secureLogin.py
@@ -1,4 +1,4 @@
-
+import random
 class User:
     def __init__(self) -> None:
         self.username = None
@@ -20,5 +20,28 @@ class User:
             with open(usernames.txt, 'w') as file:
                 file.write(user + '\n')
                 return True
+
+language = []
+language += [chr(i) for i in range(65, 91)]
+language += [chr(i) for i in range(97, 123)]
+language += [chr(i) for i in range(48,58)]
+
+
+def create_key(string):
+    key = []
+    for i in range(len(string)):
+        key.append(random.randint(0, 62))
+    return key
+
+def encrypt_password(password):
+    key = create_key(password)
+    encrypted_word = ''
+    for i in range(len(key)):
+        new_index = (language.index(password[i]) + key[i]) % 62
+        encrypted_word += language[new_index]
+    return encrypted_word
+
+print(encrypt_password('hello'))
+        
 
 
