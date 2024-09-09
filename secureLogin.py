40c9acf1191fcbc9f5370c307e4444a98187a584
charsmit <161141106+CharSmit@users.noreply.github.com>
2024-09-09 21:22:32 +0100

    Integrated Password functions into user class

--- secureLogin.py
+++ secureLogin.py
@@ -1,10 +1,16 @@
 import random
+language = []
+language += [chr(i) for i in range(65, 91)]
+language += [chr(i) for i in range(97, 123)]
+language += [chr(i) for i in range(48,58)]
+
 class User:
     def __init__(self) -> None:
         self.username = None
         self.password = None
         self.phone = None
         self.email = None
+        self.password = None
     
     def setUsername(self, user):
         try:
@@ -20,28 +26,27 @@ class User:
             with open(usernames.txt, 'w') as file:
                 file.write(user + '\n')
                 return True
+    
+    def set_password(self):
+        passw = input('Enter the password for the user')
+        self.password = self.encrypt_password(passw)
 
-language = []
-language += [chr(i) for i in range(65, 91)]
-language += [chr(i) for i in range(97, 123)]
-language += [chr(i) for i in range(48,58)]
+
+    def create_key(self, string):
+        key = []
+        for i in range(len(string)):
+            key.append(random.randint(0, 62))
+        return key
+
+    def encrypt_password(self, password):
+        key = create_key(password)
+        encrypted_word = ''
+        for i in range(len(key)):
+            new_index = (language.index(password[i]) + key[i]) % 62
+            encrypted_word += language[new_index]
+        return encrypted_word
 
 
-def create_key(string):
-    key = []
-    for i in range(len(string)):
-        key.append(random.randint(0, 62))
-    return key
 
-def encrypt_password(password):
-    key = create_key(password)
-    encrypted_word = ''
-    for i in range(len(key)):
-        new_index = (language.index(password[i]) + key[i]) % 62
-        encrypted_word += language[new_index]
-    return encrypted_word
-
-print(encrypt_password('hello'))
-        
 
 
